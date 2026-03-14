3f697547d93ca998bd74576e7c01d0bc2affb682
Alireza Bayat <dr.bayat.alireza@gmail.com>
2026-03-14 09:23:56 +0100

    setting_up_all_required_files_correctly

---
+++ src/pykochbuch/unit.py
@@ -0,0 +1,9 @@
+from enum import Enum
+class Unit(Enum):
+    GRAM=1
+    KILOGRAM=2
+    MILLILITER=3
+    LITER=4
+    PIECE=5
+    TABLESPOON=6
+    TEASPOON=7
---
+++ tests/test_unit.py
@@ -0,0 +1,4 @@
+import pytest
+from pykochbuch.unit import Unit
+def test_Unit():
+    assert Unit.GRAM == Unit.GRAM
